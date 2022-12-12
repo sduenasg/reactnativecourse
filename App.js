ba87b51eb9579138a20f67b0eab4df160ac62093
Sara Dueñas <sduenas@institutorobotica.org>
2022-12-12 17:55:37 +0100

    hello world and cats

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello world 🐈</Text>
+      <Text>hello world 🐈</Text>
+      <Text>hello world 🐈</Text>
+      <Text>hello world 🐈</Text>
+      <Text>hello world 🐈</Text>
+      <Text>hello world 🐈</Text>
       <StatusBar style="auto" />
     </View>
   );
