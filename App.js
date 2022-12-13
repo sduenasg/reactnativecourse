b316cb56b80874bc08c37f282e7b87f8fe92743d
Sara Dueñas <sduenas@institutorobotica.org>
2022-12-13 17:53:55 +0100

    separated the image picker screen to a separate file

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import {
   StyleSheet,
   Text,
@@ -10,70 +11,14 @@ import {
   Platform,
 } from "react-native";
 import slothImage from "./assets/sloth.png";
-import * as ImagePicker from "expo-image-picker";
-import * as Sharing from "expo-sharing";
-import uploadToAnonymousFilesAsync from "anonymous-files";
+
 
 export default function App() {
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  let openImagePickerAsync = async () => {
-    let permissionResult =
-      await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (permissionResult.granted === false) {
-      alert("Permission to acces camera is required");
-    } else {
-      const pickerResult = await ImagePicker.launchImageLibraryAsync();
-      if (pickerResult.canceled === false) {
-        if (Platform.OS === "web") {
-          // CORS error...
-          // let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.assets[0].uri);
-          // console.log(remoteUri);
-        } 
-        
-        setSelectedImage({ localUri: pickerResult.assets[0].uri });
-        
-      }
-    }
-  };
-
-  const openShareDialogAsync = async () => {
-    if (!(await Sharing.isAvailableAsync())) {
-      alert("Sharing is not available on your platform");
-    } else {
-      await Sharing.shareAsync(selectedImage.localUri);
-    }
-  };
-
+  
   return (
     <View style={styles.container}>
       <Image style={styles.imageIcon} source={slothImage} />
       <Text style={styles.title}>hello world 🐈</Text>
-      <TouchableOpacity onPress={openImagePickerAsync}>
-        <Image
-          style={styles.image}
-          source={{
-            uri:
-              selectedImage !== null
-                ? selectedImage.localUri
-                : "https://picsum.photos/200/200",
-          }}
-        />
-      </TouchableOpacity>
-
-      {selectedImage ? (
-        <TouchableOpacity
-          style={styles.button}
-          title="Share"
-          color="blue"
-          onPress={openShareDialogAsync}
-        >
-          <Text style={styles.buttonText}> Share 🦔 </Text>
-        </TouchableOpacity>
-      ) : (
-        <View />
-      )}
     </View>
   );
 }
