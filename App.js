0a6abc74ba21120d6f559d55c7ffc5f25cdf8237
Sara Dueñas <sduenas@institutorobotica.org>
2022-12-13 11:38:43 +0100

    buttons

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
-import slothImage from './assets/sloth.png'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import slothImage from "./assets/sloth.png";
 export default function App() {
   return (
     <View style={styles.container}>
@@ -9,10 +17,21 @@ export default function App() {
         style={styles.image}
         source={{ uri: "https://picsum.photos/200/200" }}
       />
-      <Image
-        style={styles.image}
-        source={slothImage}
+      <Image style={styles.image} source={slothImage} />
+      <Button
+        title="ZZZZzzzzZZZZ 💤😴"
+        color="cyan"
+        onPress={() => console.log("Hej sekai")}
       />
+      <Button title="Hej" color="blue" onPress={() => Alert.alert("Hej!!!")} />
+      <TouchableOpacity
+        style={styles.button}
+        title="Hej"
+        color="blue"
+        onPress={() => Alert.alert("Hej!!!")}
+      >
+        <Text style={styles.buttonText}> Press me 🐱‍👤 </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -28,9 +47,18 @@ const styles = StyleSheet.create({
     fontSize: 30,
     color: "#FFFFFF",
   },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 20
+  },
   image: {
     borderRadius: 100,
     height: 200,
     width: 200,
+  },
+  button: {
+    backgroundColor: "deepskyblue",
+    padding:7,
+    marginTop:10
   },
 });
