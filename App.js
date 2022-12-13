a8183bfffe5d23aebd09786fa4f9a51231dbfec3
Sara Dueñas <sduenas@institutorobotica.org>
2022-12-13 13:49:26 +0100

    expo image picker

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -9,28 +9,51 @@ import {
   TouchableOpacity,
 } from "react-native";
 import slothImage from "./assets/sloth.png";
+import * as ImagePicker from "expo-image-picker";
+
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  let openImagePickerAsync = async () => {
+    let permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to acces camera is required");
+    } else {
+      const pickerResult = await ImagePicker.launchImageLibraryAsync();
+      if (pickerResult.canceled === false) {
+        setSelectedImage({ localUri: pickerResult.assets[0].uri });
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
+      <Image style={styles.imageIcon} source={slothImage} />
       <Text style={styles.title}>hello world 🐈</Text>
       <Image
         style={styles.image}
-        source={{ uri: "https://picsum.photos/200/200" }}
+        source={{
+          uri:
+            selectedImage !== null
+              ? selectedImage.localUri
+              : "https://picsum.photos/200/200",
+        }}
       />
-      <Image style={styles.image} source={slothImage} />
       <Button
         title="ZZZZzzzzZZZZ 💤😴"
         color="cyan"
         onPress={() => console.log("Hej sekai")}
       />
-      <Button title="Hej" color="blue" onPress={() => Alert.alert("Hej!!!")} />
+
       <TouchableOpacity
         style={styles.button}
         title="Hej"
         color="blue"
-        onPress={() => Alert.alert("Hej!!!")}
+        onPress={openImagePickerAsync}
       >
-        <Text style={styles.buttonText}> Press me 🐱‍👤 </Text>
+        <Text style={styles.buttonText}> Image Picker🐱‍👤 </Text>
       </TouchableOpacity>
     </View>
   );
@@ -49,16 +72,21 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: "#FFFFFF",
-    fontSize: 20
+    fontSize: 20,
   },
   image: {
+    //resizeMode: "contain",
     borderRadius: 100,
     height: 200,
     width: 200,
   },
+  imageIcon: {
+    height: 50,
+    width: 50,
+  },
   button: {
     backgroundColor: "deepskyblue",
-    padding:7,
-    marginTop:10
+    padding: 7,
+    marginTop: 10,
   },
 });
