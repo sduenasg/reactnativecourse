410c10a2ac79bb0b19321a3ab0348e871a4ce13d
Sara Dueñas <sduenas@institutorobotica.org>
2022-12-13 10:50:04 +0100

    images

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
+import { StyleSheet, Text, View, Image } from "react-native";
+import slothImage from './assets/sloth.png'
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>hello world 🐈</Text>
-      <Text>hello world 🐈</Text>
-      <Text>hello world 🐈</Text>
-      <Text>hello world 🐈</Text>
-      <Text>hello world 🐈</Text>
-      <Text>hello world 🐈</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={styles.image}
+        source={{ uri: "https://picsum.photos/200/200" }}
+      />
+      <Image
+        style={styles.image}
+        source={slothImage}
+      />
     </View>
   );
 }
@@ -18,11 +20,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#292929",
     alignItems: "center",
     justifyContent: "center",
   },
-  title:{
-    fontSize: 30
-  }
+  title: {
+    fontSize: 30,
+    color: "#FFFFFF",
+  },
+  image: {
+    borderRadius: 100,
+    height: 200,
+    width: 200,
+  },
 });
