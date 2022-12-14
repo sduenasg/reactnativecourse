0957594457e4eeae0a5ebda0438d3fcd8922ad98
Sara Dueñas <sduenas@institutorobotica.org>
2022-12-14 13:34:29 +0100

    useState, list, scrollview

--- App.js
+++ App.js
@@ -10,44 +10,101 @@ import {
   TouchableOpacity,
   Platform,
   TextInput,
+  ScrollView,
 } from "react-native";
 import slothImage from "./assets/sloth.png";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    // new state depends on the previous state
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+    console.log(enteredGoalText);
+  }
+
   return (
     <View style={styles.appContainer}>
-      <Image style={styles.imageIcon} source={slothImage} />
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add Goal" />
+      <View style={styles.topImage}>
+        <Image style={styles.imageIcon} source={slothImage} />
       </View>
-      <View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          onChangeText={goalInputHandler}
+          style={styles.textInput}
+          placeholder="Your course goal!"
+        />
+        <Button onPress={addGoalHandler} title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
         <Text>List of goals...🐈</Text>
+        <ScrollView alwaysBounceVertical={false}>
+          {courseGoals.map((goal) => (
+            <View style={styles.goalItem}>
+              <Text style={styles.goalText} key={goal}>
+                {goal}
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-    paddig:50,
-    flex: 1,
-    backgroundColor: "#292929",
-    alignItems: "center",
-  },
-  inputContainer: {
-    flexDirection:'row',
-    justifyContent:'space-between'
-  },
-  textInput: {
-    borderWidth:1,
-    borderColor: "#CCCCCC",
-    width:'70%',
-    marginRight:8,
-    padding: 8
-  },
   imageIcon: {
     height: 50,
     width: 50,
+  },
+  topImage: {
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'center'
+  },
+  appContainer: {
+    flexDirection: "column",
+    paddig: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center", //default is stretch
+    padding: 24,
+    borderBottomWidth: 1,
+    borderColor: "gray",
+    marginBottom: 24,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#CCCCCC",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0ecc",
+    padding: 8,
+    color: "white",
+  },
+  goalText: {
+    color: "white",
   },
 });
